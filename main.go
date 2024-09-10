e79e0d1719fd30ded3424676bc45b336f3dbaab0
Sanjar <sanjarbakhramovich@gmail.com>
2024-09-10 07:45:31 +0500

    Added POST request, to update message global var

--- main.go
+++ main.go
@@ -1,18 +1,41 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+var message = "global variable"
+
+type MessageRequest struct {
+	Message string `json:"message"`
+}
+
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, World!")
+	fmt.Fprintf(w, "Hello, %s!", message)
+}
+
+func UpdateMessageHandler(w http.ResponseWriter, r *http.Request) {
+	var req MessageRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	message = req.Message
+	fmt.Fprintln(w, "Message updated successfully")
 }
 
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
-	http.ListenAndServe(":8080", router)
+	router.HandleFunc("/api/update-message", UpdateMessageHandler).Methods("POST")
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		fmt.Println("Error starting server", err)
+	}
 }
